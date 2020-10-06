b859b258ae324ded47203ac758755aa9d26dfddc
옥인성 <ois0531@naver.com>
2020-10-06 21:59:02 +0900

    BottomNavigationView 추가

--- app/src/main/java/com/example/digitalcontent_project/MainActivity.java
+++ app/src/main/java/com/example/digitalcontent_project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.digitalcontent_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button testbtn = findViewById(R.id.testbtn);
+        final FragmentManager fragmentmanager = getSupportFragmentManager();
+        final Fragment1 fragment1 = new Fragment1();
+        final Fragment2 fragment2 = new Fragment2();
+        final Fragment3 fragment3 = new Fragment3();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomview);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentTransaction transaction = fragmentmanager.beginTransaction();
+                switch (menuItem.getItemId()){
+                    case R.id.menu1 : {
+                        transaction.replace(R.id.framelayout,fragment1).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.menu2 : {
+                        transaction.replace(R.id.framelayout,fragment2).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.menu3 : {
+                        transaction.replace(R.id.framelayout,fragment3).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
 
         testbtn.setOnClickListener(new View.OnClickListener() {
             @Override
