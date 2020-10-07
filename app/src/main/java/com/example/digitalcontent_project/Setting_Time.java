a3e156a253eb35e4866aab7368892c6eaaa5537d
옥인성 <ois0531@naver.com>
2020-10-08 00:52:56 +0900

    Inital commit

--- app/src/main/java/com/example/digitalcontent_project/Setting_Time.java
+++ app/src/main/java/com/example/digitalcontent_project/Setting_Time.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -21,8 +22,10 @@ public class Setting_Time extends AppCompatActivity {
         setContentView(R.layout.activity_setting__time);
         TimePicker mTimePicker = findViewById(R.id.timepicker);
         Calendar mCalendar = Calendar.getInstance();
+        RadioGroup meal = findViewById(R.id.meal);
 
         int hour,min;
+        String user_meal;
 
         if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){ // TimePicker에서 시간,분을 받아옴.
             hour = mTimePicker.getHour();
@@ -32,5 +35,19 @@ public class Setting_Time extends AppCompatActivity {
             hour = mTimePicker.getCurrentHour();
             min = mTimePicker.getCurrentMinute();
         }
+
+        meal.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkid) {
+                switch (checkid){
+                    case R.id.before : {
+                        //식전체크했을때
+                    }
+                    case R.id.after : {
+                        //식후체크했을때
+                    }
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/digitalcontent_project/User_Information.java
@@ -0,0 +1,14 @@
+package com.example.digitalcontent_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__information);
+    }
+}
