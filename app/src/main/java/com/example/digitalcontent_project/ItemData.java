2a8054f083d68a4b1dd0fb99bf65162812bba596
04pys <04pys@naver.com>
2020-11-01 01:24:15 +0900

    MyAdapter ItemViewHolder부분 오류 못고침 listitem4.xml 추가

---
+++ app/src/main/java/com/example/digitalcontent_project/ItemData.java
@@ -0,0 +1,10 @@
+package com.example.digitalcontent_project;
+
+public class ItemData {
+    public ItemData(String title, String subtitle){
+        this.title=title;
+        this.subtitle=subtitle;
+    }
+    public String title;
+    public String subtitle;
+}
