323a14f5362746b4cf65f14bc2a263e6a7abb994
04pys <04pys@naver.com>
2020-11-01 23:27:21 +0900

    RecyclerView 글자입력 만들기 전 Fragment4 오류

--- app/src/main/java/com/example/digitalcontent_project/fragment/Fragment4.java
+++ app/src/main/java/com/example/digitalcontent_project/fragment/Fragment4.java
@@ -3,28 +3,44 @@ package com.example.digitalcontent_project.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.digitalcontent_project.ItemData;
+import com.example.digitalcontent_project.MyAdapter;
 import com.example.digitalcontent_project.R;
 
 
 public class Fragment4 extends Fragment {
 
-
+    private MyAdapter adapter = new MyAdapter();
 
     public Fragment4() {
         // Required empty public constructor
     }
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreat(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_4);
+
+        RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter.addItem(new ItemData("첫번째 아이템","꾸엑"));
+        adapter.addItem(new ItemData("두번째 아이템","우엑"));
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
